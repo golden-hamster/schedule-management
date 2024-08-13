c6bba39847f2182a92e95d72369b0c752fef9d9b
golden-hamster <isack19999@gmail.com>
2024-08-13 22:34:05 +0900

    Feat: 검증 구현에 따른 예외처리 추가

--- src/main/java/com/nbcamp/schedule_management/advice/ControllerAdvice.java
+++ src/main/java/com/nbcamp/schedule_management/advice/ControllerAdvice.java
@@ -6,6 +6,9 @@ import com.nbcamp.schedule_management.exception.ScheduleNotFoundException;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -30,5 +33,22 @@ public class ControllerAdvice {
     @ExceptionHandler(DataAccessException.class)
     public ResponseEntity<ErrorResponse> handleRuntimeException(DataAccessException e) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse("예상하지 못한 문제가 발생했습니다."));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+
+        StringBuilder sb = new StringBuilder();
+        for (FieldError fieldError : bindingResult.getFieldErrors()) {
+            sb.append("[");
+            sb.append(fieldError.getField());
+            sb.append("](은)는 ");
+            sb.append(fieldError.getDefaultMessage());
+            sb.append(" 입력된 값: [");
+            sb.append(fieldError.getRejectedValue());
+            sb.append("]");
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(sb.toString()));
     }
 }
